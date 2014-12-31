6cf89475816233e4654d9d84c46efb9f601264a9
Mandla Mbuli <lm.mbuli@gmail.com>
2014-12-31 20:50:46 +0200

    adding wallpaper list things

--- main.py
+++ main.py
@@ -3,10 +3,25 @@ kivy.require('1.8.0')
 
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import StringProperty
+from kivy.properties import ListProperty
+
+class PhephaListItem(BoxLayout, Label):
+    directory = StringProperty()
+    filename = StringProperty()
 
 
 class Wallpapers(Screen):
-    pass
+    data = ListProperty()
+
+    def args_converter(self, row_index, filename):
+        result = {
+            "filename": filename,
+            "directory": self.directory
+        }
+        return result
 
 
 class WallphephaApp(App):

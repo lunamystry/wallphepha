7033d8a27b0f44b1227be6909637ad701bb9172f
Mandla Mbuli <lm.mbuli@gmail.com>
2014-12-31 22:44:55 +0200

    recursively add images to wallpaper list, only allowed extensions

--- main.py
+++ main.py
@@ -8,19 +8,21 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import StringProperty
 from kivy.properties import ListProperty
 
+import os
+
+
 class PhephaListItem(BoxLayout, Label):
-    directory = StringProperty()
     filename = StringProperty()
+    image_url = StringProperty()
 
 
 class Wallpapers(Screen):
     data = ListProperty()
-    directory = StringProperty("/home/leny/Images/wallpapers")
 
-    def args_converter(self, row_index, filename):
+    def args_converter(self, row_index, item):
         result = {
-            "filename": filename,
-            "directory": self.directory
+            "filename": item['filename'],
+            "image_url": item['image_url']
         }
         return result
 
@@ -28,7 +30,20 @@ class Wallpapers(Screen):
 class WallphephaApp(App):
     def build(self):
         self.wallpapers = Wallpapers(name="wallpapers")
-        self.wallpapers.data = ["filename", "second file"]
+        phepha_dir = "/home/leny/Images/wallpapers"
+
+        def load_wallpapers(phepha_dir):
+            phepha_list = []
+            allowed_exts = ['.png', '.bmp', '.jpg', '.jpeg']
+            for dir, dirs, files in os.walk(phepha_dir):
+                for filename in files:
+                    ext = os.path.splitext(filename)[1]
+                    if ext in allowed_exts:
+                        phepha_list.append(dict(filename=filename,
+                                           image_url=os.path.join(dir,
+                                                                  filename)))
+            return phepha_list
+        self.wallpapers.data = load_wallpapers(phepha_dir)
 
         self.transition = SlideTransition(duration=.35)
         root = ScreenManager(transition=self.transition)

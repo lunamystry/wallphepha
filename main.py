4c56b712f50d38a7f0af0fe0aafcb84717bfc047
Mandla Mbuli <lm.mbuli@gmail.com>
2015-01-01 08:57:25 +0200

    lets actually set the wallpaper then

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import StringProperty
 from kivy.properties import ListProperty
 
+import subprocess
+import fileinput
+import sys
 import os
+import re
 
 
 class PhephaListItem(BoxLayout, Label):
@@ -49,6 +53,27 @@ class WallphephaApp(App):
         root = ScreenManager(transition=self.transition)
         root.add_widget(self.wallpapers)
         return root
+
+    def set_wallpaper(self, filepath):
+        print("set wallpaper: "+filepath)
+        command = "feh --bg-scale " + filepath
+        process = subprocess.Popen(command,
+                                   shell=True,
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE)
+        homepath = os.path.expanduser("~")
+        self.change_in_file(homepath+"/.xprofile", filepath)
+        process.communicate()
+
+    def change_in_file(self, filename, new_wallpaper):
+        wallpaper_command = "feh --bg-scale "
+        if os.path.isfile(filename):
+            for line in fileinput.input(filename, inplace=True):
+                if re.match(r''+wallpaper_command, line) is not None:
+                    line = wallpaper_command + new_wallpaper + "\n"
+                    sys.stdout.write(line)
+                else:
+                    sys.stdout.write(line)
 
 
 if __name__ == "__main__":

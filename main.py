87e450ae64dee996fc3ebdcc26c61b367f8e7c9a
Mandla Mbuli <lm.mbuli@gmail.com>
2015-01-01 17:02:04 +0200

    load wallpapers is independent now

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import StringProperty
 from kivy.properties import ListProperty
-from kivy.uix.label import Label
 from kivy.uix.listview import ListItemButton
 
 import subprocess
@@ -41,24 +40,24 @@ class WallphephaApp(App):
     def build(self):
         self.wallpapers = Wallpapers(name="wallpapers")
         phepha_dir = "/home/leny/Images/wallpapers"
-
-        def load_wallpapers(phepha_dir):
-            phepha_list = []
-            allowed_exts = ['.png', '.bmp', '.jpg', '.jpeg']
-            for dir, dirs, files in os.walk(phepha_dir):
-                for filename in files:
-                    ext = os.path.splitext(filename)[1]
-                    if ext in allowed_exts:
-                        phepha_list.append(dict(filename=filename,
-                                           image_url=os.path.join(dir,
-                                                                  filename)))
-            return phepha_list
-        self.wallpapers.data = load_wallpapers(phepha_dir)
+        self.load_wallpapers(phepha_dir)
 
         self.transition = SlideTransition(duration=.35)
         root = ScreenManager(transition=self.transition)
         root.add_widget(self.wallpapers)
         return root
+
+    def load_wallpapers(self, phepha_dir):
+        phepha_list = []
+        allowed_exts = ['.png', '.bmp', '.jpg', '.jpeg']
+        for dir, dirs, files in os.walk(phepha_dir):
+            for filename in files:
+                ext = os.path.splitext(filename)[1]
+                if ext in allowed_exts:
+                    phepha_list.append(dict(filename=filename,
+                                       image_url=os.path.join(dir,
+                                                              filename)))
+        self.wallpapers.data = phepha_list
 
     def set_wallpaper(self, filepath):
         command = "feh --bg-scale " + filepath
